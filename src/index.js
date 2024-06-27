95cf980d48eb961cfb798094d3126c3eac6e3da1
Ivan Ballester <irvan.baloncesto@gmail.com>
2024-06-27 15:21:04 +0200

    FT_RMT_2406_IVAN

--- src/index.js
+++ src/index.js
@@ -2,37 +2,52 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
-
-
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
+let buttonNode = document.querySelector("#start-btn");
 
-
-
+buttonNode.addEventListener("click", () => {
+  startCountdown();
+});
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
-
   // Your code goes here ...
+  let timerNode = document.querySelector("#time");
+  remainingTime = DURATION;
+
+  let interval1 = setInterval(() => {
+    document.getElementById("start-btn").disabled = true;
+    remainingTime--;
+    timerNode.innerText = remainingTime;
+
+    if (remainingTime === 9) {
+      showToast("⏰ Final countdown! ⏰");
+    } else if (remainingTime === 5) {
+      showToast("Start the engines! 💥");
+    }
+    if (remainingTime === 0) {
+      clearInterval(interval1);
+      showToast("Lift off! 🚀");
+      document.getElementById("start-btn").disabled = false;
+    }
+  }, 1000);
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
+  document.querySelector("#toast-message").innerText = message; 
+  let toastClass = document.querySelector(".toast");
+  toastClass.classList.add("show");
 
-  // Your code goes here ...
+  setTimeout(() => toastClass.classList.remove("show"), 3000);
 
-
-
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+  let xBtn = document.querySelector("#close-toast");
+  xBtn.addEventListener("click", () => {
+    toastClass.classList.remove("show");
+   
+  });
 }
